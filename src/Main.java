fd7184ac3ce2ecc28bac70924092e4c0664fbd64
GVK927 <75626986+GVK927@users.noreply.github.com>
2021-05-13 17:53:40 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main (String[] args) {
         Game game = new Game();
+        //TODO класс пешки 41-я строчка - починить первый ход(проверка на препятствие)
     }
 }
